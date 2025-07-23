3bc1dadceace64703c1425660724517d9af6d1b5
Youcef Boun <141339472+youcef-boun@users.noreply.github.com>
2025-07-23 05:32:54 +0100

    Update SupabaseModule.kt

--- app/src/main/java/com/youcef_bounaas/athlo/Authentication/di/SupabaseModule.kt
+++ app/src/main/java/com/youcef_bounaas/athlo/Authentication/di/SupabaseModule.kt
@@ -18,8 +18,8 @@ import org.koin.dsl.module
 val SupabaseModule = module {
     single {
         createSupabaseClient(
-            supabaseUrl = "https://bvyastzmsybpaxynyflg.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2eWFzdHptc3licGF4eW55ZmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5NjQyNzEsImV4cCI6MjA2NTU0MDI3MX0.PFoLliGChIzVhm3LjsNVr1lyS3cfHV97N2zJqZedr1w"
+            supabaseUrl = "Add your own url here !!",
+            supabaseKey = "Add your own key here !!"
         ) {
             install(Storage)
             install(Auth)
